c688bf12f0a6b6a54357418ec979f813cd8826e3
Florian Weinaug <florianweinaug@outlook.de>
2019-11-03 14:45:55 +0100

    implement 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,8 +7,15 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section className="hero is-primary is-bold is-fullheight">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Not found</h1>
+          <p className="subtitle">This page does not exist anymore. Sorry!</p>
+          <Link to="/" className="button is-primary is-light is-small">Home</Link>
+        </div>
+      </div>
+    </section>
   </Layout>
 )
 
