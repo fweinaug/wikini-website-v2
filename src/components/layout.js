df0172bdaa3f81b2c75bf6a2b135855840b44632
Florian Weinaug <florianweinaug@outlook.de>
2021-02-20 15:20:08 +0100

    add github link

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
 
 import "./layout.css"
 
 const Layout = ({ children }) => (
   <>
     <main>{children}</main>
-    <footer className="section has-background-white-bis py-5">
+    <footer className="section has-background-white-bis py-4">
       <div className="container">
         <nav className="level">
           <div className="level-left">
@@ -23,6 +25,10 @@ const Layout = ({ children }) => (
               <div className="buttons is-right is-uppercase are-small">
                 <Link to="/releases" className="button is-outlined">Release notes</Link>
                 <Link to="/about" className="button is-outlined">About</Link>
+                <a href="https://github.com/fweinaug/wikini" target="_blank" title="GitHub"
+                   className="button is-ghost is-normal has-text-black">
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
               </div>
             </div>
           </div>
